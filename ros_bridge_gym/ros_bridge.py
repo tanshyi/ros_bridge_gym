5e06e02e4085bdb30214a2845ca70fdb42ce4107
TS, Yin <tianshi.yin@gmail.com>
2021-10-24 21:36:26 +0800

    Add action bridge

--- ros_bridge_gym/ros_bridge.py
+++ ros_bridge_gym/ros_bridge.py
@@ -17,6 +17,31 @@ class BridgeNode(Node):
             qos_profile = 10
         )
 
+        self._action = None
+        self._action_pub = self.create_publisher(
+            msg_type = Float64MultiArray,
+            topic = 'bridge_action',
+            qos_profile = 10
+        )
+
+
     def _bridge_state(self, msg):
         self._state = np.float64(msg.data)
+
+    @property
+    def state(self):
+        return self._state
+
+
+    @property
+    def action(self):
+        return self._action
+
+    @action.setter
+    def action(self, action):
+        msg = Float64MultiArray()
+        msg.data = action
+        self._action_pub.publish(msg)
+        self._action = np.float64(action)
+
 
